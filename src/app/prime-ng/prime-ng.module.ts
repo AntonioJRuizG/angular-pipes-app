b321ba76e214b4181e2bb9d1e962e94e7ea824ee
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-18 17:45:55 +0200

    Import primeng modules

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,12 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { MenuModule } from 'primeng/menu';
 import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FieldsetModule } from 'primeng/fieldset';
+import { PanelModule } from 'primeng/panel';
 
 // Es aconsejable generar un modulo específico para PrimeNG en el que getionar todas las exportaciones de PrimeNG y mejorar la legibilidad del código.
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MenuModule, MenubarModule],
+  exports: [
+    MenuModule,
+    MenubarModule,
+    ButtonModule,
+    CardModule,
+    FieldsetModule,
+    PanelModule,
+  ],
 })
 export class PrimeNgModule {}
