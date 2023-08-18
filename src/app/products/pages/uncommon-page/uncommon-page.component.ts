c4202494ebd898b4d5231c55f3e1b3784df3a959
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-18 23:53:27 +0200

    Practice with il8nSelect & il8nPlural pipes

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -3,6 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'products-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrls: ['./uncommon-page.component.css'],
 })
-export class UncommonPageComponent {}
+export class UncommonPageComponent {
+  //il8nSelect
+  public name: string = 'Louis';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'he',
+    female: 'she',
+  };
+
+  changeClient(): void {
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
+
+  //il8nPlural
+  public clients: string[] = [
+    'Maria',
+    'Christine',
+    'Anthony',
+    'Rashel',
+    'Jean',
+  ];
+  public clientsMap = {
+    '=1': '1 client',
+    other: '# clients',
+  };
+
+  deleteClient(): void {
+    this.clients.shift();
+  }
+}
