5db7918ca62dd80f230680cf60975bd1c2f149cc
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-18 23:29:54 +0200

    Remove unused files

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'products-numbers-page',
   templateUrl: './numbers-page.component.html',
-  styleUrls: ['./numbers-page.component.css'],
 })
 export class NumbersPageComponent {
   public totalSells: number = 4234559.5567;
