d313817d9c9373c19fb32beeedf31f5a111ca086
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-20 01:42:06 +0200

    Add sortby pipe

---
+++ src/app/products/pipes/sortBy.pipe.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Artist } from '../interfaces/artist';
+
+@Pipe({
+  name: 'sortBy',
+})
+export class SortByPipe implements PipeTransform {
+  transform(artists: Artist[], sortBy?: keyof Artist | ''): Artist[] {
+    switch (sortBy) {
+      case 'name':
+        return artists.sort((a, b) => (a.name > b.name ? 1 : -1));
+      case 'isAlive':
+        return artists.sort((a, b) => (a.isAlive > b.isAlive ? 1 : -1));
+      case 'style':
+        return artists.sort((a, b) => (a.style > b.style ? 1 : -1));
+      case 'country':
+        return artists.sort((a, b) => (a.country > b.country ? 1 : -1));
+      case 'time':
+        return artists.sort((a, b) => (a.time > b.time ? 1 : -1));
+      default:
+        return artists;
+    }
+  }
+}
